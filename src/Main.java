cad43f35093e487e19d5652be6d97dd9b411f246
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-10 09:48:00 -0700

    conditional public

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,53 @@ public class Main {
     public static void go(int age) {
         if (age >= 18) {
             System.out.println("Go vote!");
-        } else{
+        } else {
             System.out.println("Go Play");
-            }
         }
-            public static void respond(char answer) {
-                if (answer == 'Y' || answer == 'y') {
-                    System.out.println("YUP YUP YUP");
-                } else {
-                    System.out.println("NOPE NOPE NOPE");
-                }
-            }
-
-            public static void drink(int age){
-                if (age < 21){
-                    System.out.println("juice");
-                }else if (age <50){
-                    System.out.println("adult beverage");
-                }else {
-                    System.out.println("tea");
-                }
-
-                }
-                public static void main(String[]args){
-                drink(11); //juice
-                drink(age=25); //adult beverage
-                drink(age=99); //tea
-
-
-                for (String a : args) {
-                    System.out.println(a);
-                }
-                go(age = 13);
-                go(age = 21);
-
-                respond(answer = 'Y');
-                respond(answer = 'N');
-            }
-        }
-
     }
+
+    public static void respond(char answer) {
+        if (answer == 'Y' || answer == 'y') {
+            System.out.println("YUP YUP YUP");
+        } else {
+            System.out.println("NOPE NOPE NOPE");
+        }
+    }
+
+    public static void drink(int age) {
+        if (age < 21) {
+            System.out.println("juice");
+        } else if (age < 50) {
+            System.out.println("adult beverage");
+        } else {
+            System.out.println("tea");
+        }
+    }
+
+    public static void main(String[] args) {
+        drink(11); //juice
+        drink(25); //adult beverage
+        drink(99); //tea
+
+
+        for (String a : args) {
+            System.out.println(a);
+        }
+
+
+    go(13);
+
+    go(21);
+
+    respond('Y');
+
+    respond('N');
+
 }
+}
+
+
+
+
 
 
