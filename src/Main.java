824379c370fd23d1f4a7bfbb5a541fbf9ac49382
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-10 09:28:23 -0700

    conditional public

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void go(int age) {
+        if (age >= 18) {
+            System.out.println("Go vote!");
+        } else {
+            System.out.println("Go Play");
         }
+        public static void respond(char answer){
+            if (answer == 'Y' || answer == 'y') {
+                System.out.println("YUP YUP YUP");
+            } else {
+                System.out.println("NOPE NOPE NOPE");
+            }
+            }
+            public static void drink(int age){
+                if (age < 21){
+                    System.out.println("juice");
+                }else if (age <50){
+                    System.out.println("adult beverage");
+                }else{
+                    System.out.println("tea");
+
+                }
+                public static void main(String[]args){
+                drink(age=11); //juice
+                drink(age=25); //adult beverage
+                drink(age=99); //tea
+
+
+                for (String a : args) {
+                    System.out.println(a);
+                }
+                go(age = 13);
+                go(age = 21);
+
+                respond(answer = 'Y');
+                respond(answer = 'N');
+            }
+        }
+
     }
 }
+
+
